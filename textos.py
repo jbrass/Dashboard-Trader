63281a70d27f4b529341bc4317a860f3e639c429
Jm <jmmartinnu@gmail.com>
2023-06-14 15:07:48 +0200

    Update 14Junio

--- textos.py
+++ textos.py
@@ -1,6 +1,6 @@
 
 txt_comentario = "Ayer Martes 13 de Junio el mercado volvió a tener un desplazamiento alcista. Con respecto a los días precederos el movimiento fue mas lento situándose el cierre de sesión en la CallWall diaria y punto de resistencia que veníamos comentando como clave."
-txt_niveles = "**Para hoy 14 de Junio:** Niveles relevantes en premercado: **SPX: Call Wall 4400 y Put Wall 4350**. y para el **ES: Call Wall 4445 y Put Wall 4395**. **Nivel Gamma 0: SPX: 4350 y ES: 4395, Estamos en gamma positiva alta.**. "
+txt_niveles = "**Para hoy 14 de Junio:** Niveles relevantes en premercado: **SPX: Call Wall 4400 y Put Wall 4350**. y para el **ES: Call Wall 4445 y Put Wall 4395**. **Nivel Gamma 0: SPX: 4330 y ES: 4375, Estamos en gamma positiva alta.**. "
 txt_sentiment = "**Sentiment** Positivo "
 txt_esperamos = "**¿Qué esperamos para hoy?** Los días de FOMC son estadísticamente días muy arrangados hasta llegar a la hora de la conferencia. Por tanto únicamente trabajaremos algún nivel relevante si lo vemos claro. "
 txt_volatilidad = "Sigue apreciándose un poco más el vix1d, mientras el vix a 30 días sigue en mínimos."
